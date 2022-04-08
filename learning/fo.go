1224c00bcf2073632635b066ced58ef975da3716
sd--Anti <service@507787.ru>
2022-04-08 19:13:46 +0300

    some formatted print output

--- learning/fo.go
+++ learning/fo.go
@@ -3,9 +3,12 @@ package main
 
 import "fmt"
 
-var n rune = 'Й'
+var a float64 = 5566.54563
+var n1 byte = 'w'
+var n2 int = 123
 
 func main() {
-
-	fmt.Printf("%q", n)
+	fmt.Printf("this is %f, type is %T", a, a)
+	fmt.Printf("\nin quotes - %q; \nbinary mode - %b;\n", n1, n2)
+	fmt.Println(a)
 }
