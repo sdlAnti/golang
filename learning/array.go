e60d74bbadba1a35e599580f8f250771b09b3bbc
sd--Anti <service@507787.ru>
2022-04-19 17:43:06 +0300

    array declaration and initialization

--- learning/array.go
+++ learning/array.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var a [5]int //объявление массива из 5 элементов
-	fmt.Print(a)
+	var a [3]int
+	a = [3]int{1, 2, 3}
+	var b [3]int = [3]int{1, 2, 3}
+	c := [3]int{1, 2, 3}
+	d := [...]int{1, 2, 3, 4, 5, 6}
+	e := [3]int{1: 12, 0: 7}
+
+	fmt.Printf("%v\n", a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 
 }
